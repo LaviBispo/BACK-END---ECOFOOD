1ce6c8205049a10e753de6cd67d09d5ae9e603ba
LiviaStockl <liviaastockl@gmail.com>
2026-09-16 10:10:08 -0300

    erro reparado pagina da tela inicial

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import userRouter from "./router/userRoutes"
 import produtoRouter from "./router/produtoRoutes"
+import telaInicioRouter from "./router/telaInicioRoutes"
 
 const app = express()
 
 app.use(express.json())
 app.use("/api/user", userRouter)
 app.use("/api/produto", produtoRouter)
+app.use("/api/telaInicio", telaInicioRouter)
+
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000")
